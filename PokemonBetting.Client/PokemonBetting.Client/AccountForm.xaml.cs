3cbbcbc49fbb374a0a88fe93f1ee57ab8bda77e9
DominicSina <dominic.sina@students.unibe.ch>
2016-10-19 11:44:17 +0200

    Basic use of HttpClient

--- PokemonBetting.Client/PokemonBetting.Client/AccountForm.xaml.cs
+++ PokemonBetting.Client/PokemonBetting.Client/AccountForm.xaml.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 //using Newtonsoft.Json;
-//using RestSharp;
 
 using Xamarin.Forms;
 
 namespace PokemonBetting.Client
 {
+
     public class Person
     {
         public String FirstName { get; set; }
@@ -31,10 +32,16 @@ namespace PokemonBetting.Client
             return null;
         }
 
-        void OnSend(object sender, EventArgs e)
+        async void OnSend(object sender, EventArgs e)
         {
             Person p = new Person { FirstName = firstNameText.Text, LastName = lastNameText.Text };
 
+
+            HttpClient httpClient = new HttpClient();
+            
+            HttpResponseMessage response = await httpClient.GetAsync("https://blogs.msdn.microsoft.com/bclteam/p/httpclient/");
+
+            firstNameText.Text = response.StatusCode.ToString();
             /*var client = new RestClient();
             client.BaseUrl = new Uri("https://github.com");
             var request = new RestRequest("/restsharp/RestSharp/wiki/Getting-Started", Method.GET);
@@ -42,6 +49,7 @@ namespace PokemonBetting.Client
             IRestResponse response = client.Execute(request);
 
             firstNameText.Text = response.StatusCode.ToString();*/
+
         }
     }
 }
