81214bb7797590b9384e0651b9161fd92452261d
Mario Kaufmann <mario.kaufmann@bluemail.ch>
2016-11-20 13:10:10 +0100

    added live battle socket

---
+++ PokemonBetting.Client/PokemonBetting.Client/Backend/LiveBattleSocket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+namespace PokemonBetting.Client.Backend
+{
+    public class LiveBattleSocket
+    {
+        public event EventHandler<string> NewMessageArrived;
+
+        public LiveBattleSocket(int battleId)
+        {
+            var webSocket = new WebSocket(GetSocketUrl(battleId));
+            webSocket.OnMessage += WebSocketOnOnMessage;
+        }
+
+        private void WebSocketOnOnMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+            NewMessageArrived?.Invoke(this, messageEventArgs.Data);
+        }
+
+        private string GetSocketUrl(int battleId)
+        {
+            return $"ws://pokemon-battle.bid/battles/{battleId}/socket.io/?EIO=2&transport=websocket";
+        }
+    }
+}
